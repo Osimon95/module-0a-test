51621978ed16a7b4dd79e484ec95c2eb00734922
Osimon95 <unotokasimon@gmail.com>
2026-06-26 19:16:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,22 +4,16 @@ from telegram import Bot
 TOKEN = "8684817654:AAG4UhWjKMxqzTRfZPeaKLWqaAAOFa5xt-s"
 CHAT_ID = "8587384068"
 
-async def heartbeat():
-    while True:
-        print("Worker is alive...")
-        await asyncio.sleep(60)
-
 async def main():
     bot = Bot(token=TOKEN)
 
     await bot.send_message(
         chat_id=CHAT_ID,
-        text="✅ Module 0B Worker Started"
+        text="✅ Module 0C Worker Started"
     )
 
-    print("Worker started successfully.")
-
-    await heartbeat()
+    while True:
+        await asyncio.sleep(300)  # Sleep for 5 minutes
 
 if __name__ == "__main__":
     asyncio.run(main())
