3a6285864bf9cc1c4247507f9995ffd7c9080d57
Osimon95 <unotokasimon@gmail.com>
2026-06-26 18:00:48 +0100

    Update bot message and add heartbeat function

--- main.py
+++ main.py
@@ -4,12 +4,22 @@ from telegram import Bot
 TOKEN = "8684817654:AAG4UhWjKMxqzTRfZPeaKLWqaAAOFa5xt-s"
 CHAT_ID = "8587384068"
 
+async def heartbeat():
+    while True:
+        print("Worker is alive...")
+        await asyncio.sleep(60)
+
 async def main():
     bot = Bot(token=TOKEN)
 
     await bot.send_message(
         chat_id=CHAT_ID,
-        text="✅ Module 0A Server Started"
+        text="✅ Module 0B Worker Started"
     )
 
-asyncio.run(main())
+    print("Worker started successfully.")
+
+    await heartbeat()
+
+if __name__ == "__main__":
+    asyncio.run(main())
