0045127183ad8848fa7f2bca45bd7fb6cbe10949
Osimon95 <unotokasimon@gmail.com>
2026-07-02 13:07:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+async def startup():
+ try:
+ await bot.send_message(
+ chat_id=CHAT_ID,
+ text="✅ Bot started on Render"
+ )
+ print("✅ Telegram startup message sent successfully.")
+ except Exception as e:
+ print(f"❌ Telegram error: {e}")
+=====================================
 Main
+
 =====================================
 async def main():
  print("🚀 Starting Telegram test...")
