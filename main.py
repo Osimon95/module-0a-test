56a7c7642deae35187a16ef493afc0c3875d227a
Osimon95 <unotokasimon@gmail.com>
2026-08-06 04:03:34 +0100

    Hard-code Telegram bot token and chat ID
    Replaced environment variable retrieval with hard-coded values for Telegram bot token and chat ID.

--- main.py
+++ main.py
@@ -19,12 +19,10 @@ SUBSCRIPTION_CHANNEL = f"{SYMBOL}@ticker"
 # Temporary hard-coded Telegram details.
 # Paste your existing token and chat ID between the normal quotation marks.
 TELEGRAM_BOT_TOKEN = os.getenv(
-    "TELEGRAM_BOT_TOKEN",
     "8684817654:AAGI7l96augCUlSaBx1xEReq7AZFfQtJhZc",
 )
 
 TELEGRAM_CHAT_ID = os.getenv(
-    "TELEGRAM_CHAT_ID",
     "8587384068",
 )
 
