2d8aa423c4675b461f8a022a387ab426227a6c4a
Osimon95 <unotokasimon@gmail.com>
2026-08-09 06:42:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ EMA200_PERIOD = 200
 # After Telegram receives the simulated confirmation message,
 # change this to False and redeploy.
 
-RUN_SIMULATED_PENDING_TEST = False
+RUN_SIMULATED_PENDING_TEST_ENABLED = False
 
 
 # ============================================================
