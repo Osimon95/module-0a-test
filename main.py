92d690fb23b70ce87088cc7adb9f1c18b0995bca
Osimon95 <unotokasimon@gmail.com>
2026-08-22 13:17:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3103,8 +3103,8 @@ async def get_demo_positions(
     ] = []
 
     endpoints = [
-        "/capi/v3/sim/position",
-        "/capi/v3/sim/positions",
+      "/capi/v3/sim/position
+      /allPosition",
     ]
 
     for path in endpoints:
