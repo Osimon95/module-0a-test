7cb79c01abe6311a53e0a2b0785b0bdeb03eb3c0
Osimon95 <unotokasimon@gmail.com>
2026-09-19 14:20:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9479,7 +9479,7 @@ async def run_r36f12():
                     "LONG_ZERO_PLANNED_QUANTITY"
                 )
 
-        real_long_snapshot = (
+real_long_snapshot = (
     build_net_roi_tp_snapshot(
         MARK_PRICE,
         long_quantity,
